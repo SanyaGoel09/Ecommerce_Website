6f55c21ee6c1b4ba157a48bbdf12c863dd1fd0d3
SanyaGoel09 <goelsanya09@gmail.com>
2023-12-21 17:01:20 +0530

    initial commit

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const [MobileMenu , setMobileMenu] = useState(false)
   return (
     <>
       <header className="header">
         <div className="container d_flex">
             <div className="catgories d_flex">
                 <span className="fa-solid fa-border-all"></span>
-                <h4>Categories <i className="fa fa-chevron-down"></i>
+                <h4>
+                  Categories <i className="fa fa-chevron-down"></i>
                 </h4>
             </div>
+
             <div className="navlink">
-                <ul className='nav'>
+                <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
                     <li> 
-                        <Link></Link></li>
+                      <Link to='/'>home</Link>
+                    </li>
+                    <li> 
+                      <Link to='/pages'>pages</Link>
+                    </li>
+                    <li> 
+                      <Link to='/user'>user account</Link>
+                    </li>
+                    <li> 
+                      <Link to='/vendor'>vendor account</Link>
+                    </li>
+                    <li> 
+                      <Link to='/track'>track my order</Link>
+                    </li>
+                    <li> 
+                      <Link to='/contact'>contact</Link>
+                    </li>
                 </ul>
+                <button className='toggle' onClick={() => setMobileMenu(!MobileMenu)}>
+                  {MobileMenu? <i className='fas fa-times close home-bth'></i> :
+                    <i className="fa-solid fa-xmark open"></i> 
+                  }
+                </button>
             </div>
         </div>
       </header>
--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import logo from '../assests/logo.png'
+import { Link } from 'react-router-dom'
 
 const Search = () => {
+  window.addEventListener("scroll", function(){
+    const search = document.querySelector(".search")
+    search.classList.toggle("active", this.window.scrollY > 100)
+  })
   return (
-    <div>
-      <h1>Search</h1>
-    </div>
+    <>
+      <section className='search'>
+        <div className="container c_flex"> 
+        <div className="logo width">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="search-box f_flex">
+          <i className="fa fa-search"></i>
+          <input type="text" placeholder="text Search and hit enter..." />
+          <span>All Category</span>
+        </div>
+
+        <div className="icon f_flex width">
+          <i className="fa fa-user icon_circle"></i>
+          <div className="cart">
+            <Link to="/cart">
+              <i className="fa fa-shopping-bag icon-circle"></i>
+              <span>0</span>
+            </Link>
+          </div>
+        </div>
+        </div>
+      </section>
+    </>
   )
 }
 
